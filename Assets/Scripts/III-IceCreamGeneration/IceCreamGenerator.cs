0ee0eeb545fd5c0caacebac67e72c7857ce588c5
arthur-lemo1ne <arthur.lemoine@algosup.com>
2022-05-13 10:38:22 +0200

    Added relief to Cornet

--- Assets/Scripts/III-IceCreamGeneration/IceCreamGenerator.cs
+++ Assets/Scripts/III-IceCreamGeneration/IceCreamGenerator.cs
@@ -82,7 +82,7 @@ public class IceCreamGenerator : MonoBehaviour
         {
             float theta = i * 2 * Mathf.PI / resolution;
             // Vertices calculation
-            vertices[i] = new Vector3(radius * Mathf.Cos(theta), height, radius * Mathf.Sin(theta));
+            vertices[i] = new Vector3(radius * Mathf.Cos(theta), height+(Mathf.Cos(theta*10)/10), radius * Mathf.Sin(theta));
 
             // Triangles calcuation
             tris[6 * i] = resolution;
